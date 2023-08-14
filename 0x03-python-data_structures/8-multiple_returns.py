62d01275d3bee1f36e46f6cb180bfb99a51f90a0
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-14 20:29:24 +1000

    return the length of string and the first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     str_len = len(sentence)
     if sentence == "":
-        return (0, 0)
+        return (0, None)
     else:
         return (str_len, sentence[0])
