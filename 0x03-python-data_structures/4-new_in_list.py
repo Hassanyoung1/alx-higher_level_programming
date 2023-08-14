fe347f483d49d08d3a88b96591913e6817a09ccb
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-14 10:36:25 +1000

    copy of elementwq

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,6 @@ def new_in_list(my_list, idx, element):
         new_list = my_list.copy()
     if idx == 0 or idx >= len(my_list):
         return new_list
-    
     else:
         my_list[idx] = element
         return new_list
