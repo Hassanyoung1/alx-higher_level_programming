723840a354e38abc32339a305a2ecac0e350a437
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-17 10:59:54 +1000

    function that returns a set of all elements present in only one set.

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,11 +2,11 @@ def only_diff_elements(set_1, set_2):
     unique_elements = set()
 
     for element in set_1:
-        if element not in set_2:
+        if element != set_2:
             unique_elements.add(element)
 
     for element in set_2:
-        if element not in set_1:
+        if element != set_1:
             unique_elements.add(element)
 
     return unique_elements
