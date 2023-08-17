128daab1a13ee544989fa0427dfd2f1f764f79d7
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-17 20:28:08 +1000

    function that deletes keys with a specific value in a dictionary.

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+def complex_delete(a_dictionary, value):
+    if value in a_dictionary.values():
+        # Create a new dictionary to store non-matching key-value pairs
+        new_dictionary = {}
+
+        for key, val in a_dictionary.items():
+            if val != value:
+                new_dictionary[key] = val
+
+        return new_dictionary
+
+    return a_dictionary  # Value not found, return the original dictionary
