c7a0cf3b373d5fcc84ec0e6fd7a8270ab2cc721d
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-29 13:55:47 +1000

    a function that raises a name exception with a message.

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a custom message.
+
+ def raise_exception_msg(message=""):
+    """
+    raise NameError(message)
