cc57a0ccbcbe42e032c7586719de5d55c33ca4e5
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-31 12:13:00 +1000

    a class Square that defines a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,14 +1,31 @@
+#!/usr/bin/python3
+
+""" Defines a square class """
+
 class Square:
+    """
+    This class defines a square by its size and position.
+    """
+
     def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes the square.
+
+        Args:
+            size (int): Given size of the square.
+            position (tuple): Given position of the square.
+        """
         self.size = size
         self.position = position
 
     @property
     def size(self):
+        """ Returns the current square size """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Setter for the square size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -17,10 +34,12 @@ class Square:
 
     @property
     def position(self):
+        """ Retrieves current position value """
         return self.__position
 
     @position.setter
     def position(self, value):
+        """ Sets the value for position """
         if (
             not isinstance(value, tuple)
             or len(value) != 2
@@ -31,9 +50,11 @@ class Square:
         self.__position = value
 
     def area(self):
+        """ Returns the current square area """
         return self.__size * self.__size
 
     def my_print(self):
+        """ Prints a representation of the square """
         if self.size == 0:
             print()
             return
@@ -45,3 +66,7 @@ class Square:
         for _ in range(self.size):
             print(row)
 
+# Example usage
+if __name__ == "__main__":
+    my_square = Square(4, (1, 2))
+    my_square.my_print()
