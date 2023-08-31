61792220ddeffb7d495043da1b678f43a3137724
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-08-31 10:53:42 +1000

    a class Square that defines a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+""" Defines a square class """
+
+
+class Square:
+    """
+    This class defines a square by its size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the square.
+
+        Args:
+            size (int): Given size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Returns the current square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for the square size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the current square area.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ print square with #"""
+        if self.size == 0:
+            print()
+            return
+        else:
+            for hash in range(self.size):
+                i = self.size
+                while i > 0:
+                    print("#", end="")
+                    i -= 1
+                print()
