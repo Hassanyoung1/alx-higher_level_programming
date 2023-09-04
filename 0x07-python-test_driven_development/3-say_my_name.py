1bef912de736426840233a4ee6f09c87f25f4767
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-04 21:38:01 +1000

    square test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the name in the format: My name is <first name> <last name>.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to an empty string.
+
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        print(f"My name is {first_name} {last_name}")
+    else:
+        print(f"My name is {first_name}")
+
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """
+    Prints a square of size 'size' with '#' character.
+
+    Args:
+        size (int): The size of the square's side.
+
+    Raises:
+        TypeError: If size is not an integer or is less than 0, with the appropriate message.
+
+    Returns:
+        None
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        return
+
+    for _ in range(size):
+        print("#" * size)
