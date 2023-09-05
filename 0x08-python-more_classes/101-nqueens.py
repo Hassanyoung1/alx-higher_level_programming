be5fd9f7454c9eeb294d7307d19ffd1461c60d35
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-06 07:42:27 +1000

    nqueen

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Finds solutions to the N-queens puzzle."""
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
