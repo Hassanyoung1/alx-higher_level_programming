fbd5ee45706563cfe5a69b93f16dc2efb08dd9a9
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-05 23:24:15 +1000

    1-rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,6 +2,7 @@
 
 """" Define an class triangle """
 
+
 class Rectangle:
     """
     Define a class Rectangle.
@@ -53,6 +54,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-
-
