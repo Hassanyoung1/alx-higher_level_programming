547f536708ec0a584d335c7faadf0cb759081c2a
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-06 06:45:33 +1000

    9 -rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -97,7 +97,7 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the rectangle with the char
+        """Return a string representation of the rectangle with the cha """
         if self.__width == 0 or self.__height == 0:
             return ""
         rectangle_str = ""
