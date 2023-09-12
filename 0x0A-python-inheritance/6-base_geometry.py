97100f80eee0e6a0c5be195a68d8ee7f3b6bb0d5
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-12 11:21:20 +1000

    a class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" Declare a class """
+
+class AreaNotImplemented(Exception):
+    """Custom exception to indicate that the area() method is not implemented."""
+    pass
+
+class BaseGeometry:
+    """
+    This is the BaseGeometry class docstring.
+    You can provide information about the class and its purpose here.
+    """
+
+    def area(self):
+        """
+        This method is not implemented in the BaseGeometry class.
+        Subclasses should override this method to calculate the area.
+        """
+        raise Exception("area() is not implemented")
+
