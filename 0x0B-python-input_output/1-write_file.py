ed04989aedfae5191ace38017c3fe6fddd39d07a
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-14 08:58:41 +1000

    function that write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def write_file(filename="", text=""):
+    """
+    Write text to a file with the specified filename.
+
+    :param filename: The name of the file to write to
+    (default is an empty string).
+    :param text: The text to be written to the file
+    (default is an empty string).
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        file_content = file.write(text)
+        return file_content
