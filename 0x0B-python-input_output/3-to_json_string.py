a3ff36b538d28f9c2ac650350b9d03d9d97a8fdd
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-14 09:41:56 +1000

    functin to return json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
 """" Declare function prototype """
+
+
 import json
+
 def to_json_string(my_obj):
+    """
+    Convert a Python object to its JSON representation as a string.
+
+    :param my_obj: The Python object to be converted to JSON.
+    :return: A JSON-formatted string representing the Python object.
+    """
     return json.dumps(my_obj)
 
