daac37413ec88750d3a7497b765b50a5db5c6a7f
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-14 08:44:40 +1000

    function that reads file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Declare the function prototype """
+
+def read_file(filename=""):
+    """
+    Read a text file (UTF-8 encoded) and print its content to stdout.
+
+    :param filename: The name of the file to read (default is an empty string).
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        file_contents = file.read()
+        print(file_contents, end="")
