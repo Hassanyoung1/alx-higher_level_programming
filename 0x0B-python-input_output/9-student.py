9bfeb484c175ab8a813f9043d15c39987c159153
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-14 12:28:03 +1000

    a class Student that defines a student by

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""Defines a class Student."""
+
+
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the Student object.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        return self.__dict__
