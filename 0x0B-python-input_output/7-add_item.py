85878c2f6755aa3f643da698c538fcc9e2275866
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-09-14 11:46:02 +1000

    script that adds all arguments to a Python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -18,8 +18,8 @@ from os.path import exists
 if __name__ == "__main__":
     # Import the necessary functions from external modules
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file =
+        __import__('6-load_from_json_file').load_from_json_file
 
     my_list = []
 
