3c0e1b4c30e284f9b2713e4e0f131ece2a79364b
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-15 03:10:34 +1000

    print

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const x = +process.argv[2];
+
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
