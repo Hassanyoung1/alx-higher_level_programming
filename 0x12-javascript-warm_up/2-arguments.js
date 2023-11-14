eca8e821b7763b58947fce64a663a97067b6b475
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-14 13:21:57 +1000

    proceess

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2) {
+	console.log('No arguement')
+}
+else if (process.argv.length === 3) {
+	console.log('Arguement found')
+}
+else 
+{
+	console.log('Arguements found')
+};
