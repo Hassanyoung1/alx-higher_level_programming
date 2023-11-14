3841d0809b4827fea2667d66d2d6fc88421e48f7
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-14 21:04:11 +1000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv.length === 2) {
-	console.log('No arguement')
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (process.argv.length === 3) {
-	console.log('Arguement found')
-}
-else 
-{
-	console.log('Arguements found')
-};
