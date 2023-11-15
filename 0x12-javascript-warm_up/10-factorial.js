7200441ddaa7fc500834d2f9c47af8139e2ca663
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-15 11:15:50 +1000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,22 @@
 #!/usr/bin/node
 
-const factorial = (n) => {
-  if (isNaN(n) || n < 0) {
+// Function to compute factorial recursively
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1; // Factorial of NaN is 1
+  }
+
+  // Base case: factorial of 0 is 1
+  if (n === 0) {
     return 1;
   }
 
-  if (n === 0 || n === 1) {
-    return 1;
-  }
+  // Recursive case: n! = n * (n-1)!
   return n * factorial(n - 1);
-};
+}
 
-const input = +process.argv[2];
+// Get the input from the command line argument
+const arg = process.argv[2];
 
-console.log(factorial(input));
+// Compute and print the factorial
+console.log(factorial(parseInt(arg)));
