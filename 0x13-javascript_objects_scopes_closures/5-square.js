82738a417c625687ac92a2fb0263d1dabb2cbd1a
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-16 21:58:11 +1000

    Square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
- /*
-  *a class Square that defines
-  *a square and inherits from Rectangle of 4-rectangle.js
-  */
+/*
+*a class Square that defines
+*a square and inherits from Rectangle of 4-rectangle.js
+*/
 
 const Rectangle = require('./4-rectangle');
 
