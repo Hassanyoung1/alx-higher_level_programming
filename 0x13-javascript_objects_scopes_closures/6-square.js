b1a0df2d4983709b816dad428fbbedd142ff2e48
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-16 22:30:00 +1000

    Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+/*
+*a class Square that defines
+*a square and inherits from Rectangle of 4-rectangle.js
+*/
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  /* Create an instance method called
+*charPrint(c) that prints
+*the rectangle using the character c
+*/
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += c;
+      }
+      console.log(line);
+    }
+  }
+}
+
+module.exports = Square;
