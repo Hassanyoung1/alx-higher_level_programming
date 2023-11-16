65bd1b67b1e6374fda051e594256ec8889b3dd32
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-16 13:22:36 +1000

    Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/**
+ * An empty class Rectangle that define a rectangle
+ * You must use the class notation for defining your class
+ *i
+ *run the main test
+ *
+ */
+
+class Rectangle {
+  /** export  class rectangle **/
+  constructor (w, h) {
+    /**
+    *defining class constructor
+    *width: Initialize the instance attribute width with the value of w
+    *height: Initialize the instance attribute height with the value of h
+    */
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+    print() {
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for  (let j = 0; j < this.width; j++) {
+          line += 'X';
+        }
+        console.log(line);
+      }
+    }
+  }
+
+module.exports = Rectangle;
