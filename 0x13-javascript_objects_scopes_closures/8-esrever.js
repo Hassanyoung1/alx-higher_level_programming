5ab8790a553a24ba839f643b04c12e08a31a6c94
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-17 03:05:58 +1000

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,9 +4,11 @@
  * @list: the list of array
 */
 exports.esrever = function (list) {
+  // Create an empty array to store the reversed elements
   const revList = [];
-
+  // itirate
   for (let i = list.length - 1; i >= 0; i--) {
+    // Add each element to the new reversed list
     revList.push(list[i]);
   }
   return revList;
