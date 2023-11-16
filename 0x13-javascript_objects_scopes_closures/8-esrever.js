2b8457f2fbfee2b6b0af37d478f07a007105c310
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-17 03:02:23 +1000

    8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ * a function that returns the reversed version of a list:
+ * @list: the list of array
+*/
+exports.esrever = function (list) {
+  const revList = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    revList.push(list[i]);
+  }
+  return revList;
+};
