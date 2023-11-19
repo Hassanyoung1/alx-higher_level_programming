09f2b24cae484e6040a7ef5ff036eff076b2ec44
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-19 17:43:04 +1000

    Filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 Script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
 
-import MySQLdb
 import sys
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    """
-    checking the length of the argurment
-    """
+
+    """checking the length of the argurment"""
 
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print(
+            "Usage: {} <username> <password> <database> <state_name>".format(
+                sys.argv[0]))
         sys.exit(1)
 
     username, password, database, state_name = sys.argv[1:5]
 
     """ Connect to MySQL database """
+
     db = MySQLdb.connect(
         host="localhost",
         user=username,
@@ -29,20 +32,26 @@ if __name__ == "__main__":
     )
 
     """ Create a cursor object using cursor() method """
+
     cursor = db.cursor()
 
     """ Execute the SQL query """
+
     cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
                    (state_name,))
 
     """ Fetch all the rows using fetchall() method """
+
     data = cursor.fetchall()
 
     """ Print the result """
+
     for row in data:
         print(row)
     """ Close the cursor """
+
     cursor.close()
 
     """ Close the database connection """
+
     db.close()
