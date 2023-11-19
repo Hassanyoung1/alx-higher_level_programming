3d58b0938f8cf11e9670a4208656c2daf41f7658
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-19 17:02:11 +1000

    safe

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,54 +1,21 @@
 #!/usr/bin/python3
 
+""" takes an argument and displays all values from database hbtn_0e_0_usa.
+    This is secure of SQL injections
 """
-Script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
-"""
-
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-
-    """checking the length of the argurment"""
-
-    if len(sys.argv) != 5:
-        print(
-            "Usage: {} <username> <password> <database> <state_name>".format(
-                sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database, state_name = sys.argv[1:5]
-
-try:
-    """ Connect to MySQL database """
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
-
-    """ Create a cursor object using cursor() method """
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306)
     cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    cursor.execute(query, (argv[4],))
+    states = cursor.fetchall()
 
-    """ Execute the SQL query """
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                   (state_name,))
+    for state in states:
+        print(state)
 
-    """ Fetch all the rows using fetchall() method """
-    data = cursor.fetchall()
-
-    """ Print the result """
-    for row in data:
-        print(row)
-except MySQLdb.Error as e:
-    print(f"MySQL Error: {e}")
-
-finally:
-    """ Close the cursor """
     cursor.close()
-
-    """ Close the database connection """
     db.close()
