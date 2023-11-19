2422c3e1c5d8433c7c6da75721d66d0b71e6e575
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-20 02:31:18 +1000

    All

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects from the database
+ use the module SQLAlchemy
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.sql import select
+
+if __name__ == "__main__":
+
+    """ Parse command-line arguments """
+    if len(sys.argv) != 4:
+        print("Usage: ./7-model_state_fetch_all.py <username> <password>\
+                <database_name>")
+        sys.exit(1)
+
+    """ Create SQLAlchemy engine """
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+
+    """  Create database tables """
+    Base.metadata.create_all(engine)
+
+    """ Execute select query and print results """
+    with engine.connect() as conn:
+        s = select(State)
+        result = conn.execute(s)
+        for row in result:
+            print("{}: {}".format(row[0], row[1]))
