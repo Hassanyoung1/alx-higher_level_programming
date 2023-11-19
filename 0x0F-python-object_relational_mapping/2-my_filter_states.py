e4e09dac6479e750bc540fa1c95acb117c687cd3
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-19 17:50:44 +1000

    Filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,6 @@
 Script that takes in an argument and displays all values
 in the states table of hbtn_0e_0_usa where name matches the argument.
 """
-
 import sys
 import MySQLdb
 from sys import argv
@@ -32,7 +31,6 @@ if __name__ == "__main__":
     )
 
     """ Create a cursor object using cursor() method """
-
     cursor = db.cursor()
 
     """ Execute the SQL query """
