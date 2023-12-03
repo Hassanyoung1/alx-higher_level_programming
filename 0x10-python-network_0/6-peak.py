1e2a9f27eb3bde1245402a267bbc11b4587f88dd
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-03 10:30:21 -0800

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/bash/python3
 """ Function declaration """
 
+
 def find_peak(list_of_integers):
     """
     Find a peak in a list of unsorted integers using binary search.
@@ -20,11 +21,9 @@ def find_peak(list_of_integers):
     while low < high:
         mid = (low + high) // 2
 
-
         if list_of_integers[mid] > list_of_integers[mid + 1]:
             high = mid
         else:
             low = mid + 1
 
     return list_of_integers[low]
-
