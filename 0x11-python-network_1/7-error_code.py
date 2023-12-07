6a948a235d488de74c1299c88ae2939054c0dfac
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-07 03:13:10 -0800

    sends a request to the URL and displays

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL,
+sends a request to the URL and displays 
+the body of the response.
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
