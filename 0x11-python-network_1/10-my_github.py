8a4a190024809710c3d8906e1f32056877a2838d
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-07 08:22:47 -0800

    takes your GitHub credentials

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-A Python script that takes your GitHub credentials 
-(username and password) and 
+A Python script that takes your GitHub credentials
+(username and password) and
 uses the GitHub API to display your id
 """
 if __name__ == "__main__":
