205685bb0bba11ef90ab1f71b7210f5b1d39bef1
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-07 06:46:55 -0800

    a letter and sends a POST request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,40 +1,24 @@
 #!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
 """
-A Python script that takes in a letter and sends
-a POST request to http://0.0.0.0:5000/search_user with
-the letter as a parameter.
-"""
+import sys
+import requests
+
+
 if __name__ == "__main__":
-    import requests
-    from sys import argv
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    # Check if the letter is provided as a command line argument
-    if len(argv) == 2:
-        letter = argv[1]
-    else:
-        letter = ""
-
-    url = "http://0.0.0.0:5000/search_user"
-    params = {'q': letter}
-
-    response = requests.post(url, params=params)
-
-    # Check the HTTP status code
-    if response.status_code == 200:
-        try:
-            # Attempt to parse the JSON response
-            json_data = response.json()
-
-            if isinstance(json_data, list) and json_data:
-                # Display the first result
-                user = json_data[0]
-                user_id = user.get('id', '')
-                user_name = user.get('name', '')
-                print(f"[{user_id}] {user_name}")
-            else:
-                print("No result")
-
-        except ValueError:
-            print("Not a valid JSON")
-    else:
-        print(f"Error: {response.status_code}")
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
