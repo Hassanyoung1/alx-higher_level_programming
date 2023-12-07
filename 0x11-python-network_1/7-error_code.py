d406375d9e15fbfa543881cd3d9c50402e214867
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-07 03:15:01 -0800

    sends a request to the URL and displays

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 a Python script that takes in a URL,
-sends a request to the URL and displays 
+sends a request to the URL and displays
 the body of the response.
 """
 if __name__ == "__main__":
