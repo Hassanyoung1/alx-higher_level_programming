6d0e10cab337230bc4920f5ca3667bfec914e978
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-12-07 23:58:40 -0800

    list 10 commits (from the most recent to oldest)

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
-"""
- a Python script that takes 2 arguments in order to solve this challenge.
-"""
+
+"""Lists 10 commits from a given github repo"""
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
-    if len
-
-
+    user = argv[2]
+    repo = argv[1]
+    req = requests.get(f'https://api.github.com/repos/{user}/{repo}/commits')
+    try:
+        commits = req.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    except ValueError:
+        pass
