5f4d5daec4d528e2119416abf22e7798b306f214
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-06 03:41:52 +1000

    Status

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/**
+ * File Write Script
+ *
+ * This script writes the HTTP status code of a specified URL provided as a command-line argument.
+ * It uses the Node.js 'request' module to make an HTTP GET request.
+ */
+
+/* Get the URL from the command-line arguments */
+const request = require("request");
+const url = process.argv[2];
+
+/* Check if a URL is provided */
+if (!url) {
+  console.error('Specify the URL');
+  process.exit(1);
+}
+
+/* Make an HTTP GET request to the specified URL */
+request(url, function (error, response) {
+  /*  Check for errors and log the HTTP status code */
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else {
+    console.log(`HTTP status code: ${response.statusCode}`);
+  }
+});
+
