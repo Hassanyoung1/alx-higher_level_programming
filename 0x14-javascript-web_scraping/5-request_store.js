106badae384ffc705fd54fc567e37a82db448873
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-06 12:32:12 +1000

    a

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,14 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
 const fs = require('fs');
 const request = require('request');
 
 const url = process.argv[2];
 const file = process.argv[3];
+if (!url) {
+  console.error('Please provide API URL of the Star Wars API: https://swapi-api.alx-tools.com/api/films/');
+  process.exit(1);
+}
 
 request(url, function (error, response, body) {
   if (error) {
