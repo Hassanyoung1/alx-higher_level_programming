f37753e0e455163a982c8d54cc2c91c103b71739
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-06 12:06:26 +1000

    the number of movies where the character is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,44 +1,38 @@
 #!/usr/bin/node
 /**
- * Wedge Antilles Movie Count Script
- *
- * This script fetches and prints the number of Star Wars movies where the character "Wedge Antilles" is present.
- * It uses the Node.js 'request' module to make an HTTP GET request to the Star Wars API.
+ * This script queries the Star Wars API (https://swapi-api.alx-tools.com/api/films/) 
+ * and prints the number of movies in which the character “Wedge Antilles” is present.
  */
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
-
-if (!apiUrl) {
-  console.error('Specify the Star Wars API URL');
+/* Check if the API URL is provided as a command-line argument */
+const url = process.argv[2];
+if (!url) {
+  console.error('Please provide API URL of the Star Wars API: https://swapi-api.alx-tools.com/api/films/');
   process.exit(1);
 }
 
-const characterId = 18;
-
-request(apiUrl, function (error, response, body) {
+/* Make a request to the provided API URL */
+request(url, (error, response, body) => {
   if (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(error);
     process.exit(1);
   }
 
-  if (response.statusCode !== 200) {
-    console.error(`Request failed with status code ${response.statusCode}`);
-    process.exit(1);
-  }
+  /* Parse the response body to extract film data */
+  const films = JSON.parse(body).results;
+  let count = 0;
 
-  const filmsData = JSON.parse(body);
-  let moviesWithWedgeCount = 0;
-
-  const isWedgePresent = (film) =>
-    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-
-  for (const film of filmsData.results) {
-    if (isWedgePresent(film)) {
-      moviesWithWedgeCount++;
+  /* Iterate through each film and its characters to count occurrences of "Wedge Antilles" */
+  for (const film of films) {
+    for (const character of film.characters) {
+      if (character.endsWith('/18/')) {
+        count++;
+      }
     }
   }
 
-  console.log(`${moviesWithWedgeCount}`);
+  /* Print the total number of movies featuring "Wedge Antilles" */
+  console.log(count);
 });
