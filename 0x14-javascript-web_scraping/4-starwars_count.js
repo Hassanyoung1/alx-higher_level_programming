fd538ba10b7af6c2743eaa90f6caf9237b96be2f
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-06 11:08:41 +1000

    the

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+/**
+ * Wedge Antilles Movie Count Script
+ *
+ * This script fetches and prints the number of Star Wars movies where the character "Wedge Antilles" is present.
+ * It uses the Node.js 'request' module to make an HTTP GET request to the Star Wars API.
+ */
+
+/* Import the 'request' module */
+const request = require('request');
+
+/* Get the API URL from the command-line arguments */
+const apiUrl = process.argv[2];
+
+/* Check if an API URL is provided */
+if (!apiUrl) {
+  console.error('Specify the Star Wars API URL');
+  process.exit(1);
+}
+
+/* Character ID for Wedge Antilles */
+const characterId = 18;
+
+/* Make an HTTP GET request to the Star Wars API */
+request(apiUrl, function (error, response, body) {
+  /* Check for errors */
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else {
+    /* Parse the JSON response */
+    const filmsData = JSON.parse(body);
+
+    /* Initialize a counter for movies with Wedge Antilles */
+    let moviesWithWedgeCount = 0;
+
+    /* Function to check if Wedge Antilles is present in a film */
+    const isWedgePresent = (film) =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+
+    /* Count movies with Wedge Antilles */
+    filmsData.results.forEach((film) => {
+      if (isWedgePresent(film)) {
+        moviesWithWedgeCount++;
+      }
+    });
+
+    /* Print the number of movies with Wedge Antilles */
+    console.log(`${moviesWithWedgeCount}`);
+  }
+});
