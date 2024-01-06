30de0c66945d3a0cb55c9a2edeb7dd4af55364c3
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-06 11:50:47 +1000

    the number of movies where the character is present.

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 /**
  * Wedge Antilles Movie Count Script
  *
@@ -6,45 +6,39 @@
  * It uses the Node.js 'request' module to make an HTTP GET request to the Star Wars API.
  */
 
-/* Import the 'request' module */
 const request = require('request');
 
-/* Get the API URL from the command-line arguments */
 const apiUrl = process.argv[2];
 
-/* Check if an API URL is provided */
 if (!apiUrl) {
   console.error('Specify the Star Wars API URL');
   process.exit(1);
 }
 
-/* Character ID for Wedge Antilles */
 const characterId = 18;
 
-/* Make an HTTP GET request to the Star Wars API */
 request(apiUrl, function (error, response, body) {
-  /* Check for errors */
   if (error) {
     console.error(`Error: ${error.message}`);
-  } else {
-    /* Parse the JSON response */
-    const filmsData = JSON.parse(body);
-
-    /* Initialize a counter for movies with Wedge Antilles */
-    let moviesWithWedgeCount = 0;
-
-    /* Function to check if Wedge Antilles is present in a film */
-    const isWedgePresent = (film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
-
-    /* Count movies with Wedge Antilles */
-    filmsData.results.forEach((film) => {
-      if (isWedgePresent(film)) {
-        moviesWithWedgeCount++;
-      }
-    });
-
-    /* Print the number of movies with Wedge Antilles */
-    console.log(`${moviesWithWedgeCount}`);
+    process.exit(1);
   }
+
+  if (response.statusCode !== 200) {
+    console.error(`Request failed with status code ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const filmsData = JSON.parse(body);
+  let moviesWithWedgeCount = 0;
+
+  const isWedgePresent = (film) =>
+    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+
+  for (const film of filmsData.results) {
+    if (isWedgePresent(film)) {
+      moviesWithWedgeCount++;
+    }
+  }
+
+  console.log(`${moviesWithWedgeCount}`);
 });
